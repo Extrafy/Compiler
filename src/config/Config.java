bb7e1d2df59b9ad8d2f79d07c2a4adffc2030fa7
wfy <1712619114@qq.com>
2025-01-23 14:51:44 +0800

    词法分析

---
+++ src/config/Config.java
@@ -0,0 +1,10 @@
+package config;
+
+public class Config {
+    public static String inputPath = "testfile.txt";
+    public static String outputPath = "lexer.txt";
+    public static String errorPath = "error.txt";
+    public static boolean lexerFlag = true;
+    public static boolean parserFlag = false;
+    public static boolean errorFlag = false;
+}
